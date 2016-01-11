f946525bf03cddad6619f02a0491521a18d5c28d
xiongxiong109 <444122662@qq.com>
2016-01-11 22:40:36 +0800

    更新分页相关作业

--- public/js/practice/paginator.js
+++ public/js/practice/paginator.js
@@ -1,12 +1,17 @@
 define('/js/practice/paginator',['bootstrap','paginator'],
 function(require,exports,module){
+	var oPage=$(".page");
 	//分页配置项
 	var options={
-		currentPage:2,
-		totalPages:5,
-		numberOfPages:5
+		currentPage:oPage.data('curpage'),
+		totalPages:oPage.data('total'),
+		numberOfPages:oPage.data('number'),
+		bootstrapMajorVersion:3,
+		onPageClicked: function(e,originalEvent,type,page){
+			window.location.href=['/practice/paginator?page=',page].join('');
+    }
 	};
 	require.async(['bootstrap','paginator'],function(){
-		$(".page").bootstrapPaginator(options);
+		oPage.bootstrapPaginator(options);
 	});
 });
--- routes/practice.js
+++ routes/practice.js
@@ -1,4 +1,5 @@
 var express=require('express');
+var mongoose=require('mongoose');
 var router=express.Router();
 
 router.get('/bs_select',function(req,res){
@@ -6,6 +7,33 @@ router.get('/bs_select',function(req,res){
 });
 
 router.get('/paginator',function(req,res){
-	res.render('practice/paginator');
+	//通过?page=id获取查询参数
+	var p=req.query.page || 1;
+	var len=0,limit=5;
+	var list=require('../models/list');
+	list.find({},function(err,total){
+		if(err) throw new Error(err);
+		len=total.length;
+		var maxP=Math.ceil(len/limit);
+		p = p>=maxP ? maxP : p;
+		
+		//查询所有数据到视图中
+		var options={
+			page:{
+				num:p,
+				limit:limit
+			}
+		}
+		list.findByPage(options,function(err,data){
+			if(err) throw new Error(err);
+			res.render('practice/paginator',{
+				data:data,
+				totalPage:maxP,
+				curPage:p,
+				number:limit
+			});
+		});
+
+	})
 });
 module.exports=router;
